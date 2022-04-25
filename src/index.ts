8447c1185b6853ddaf61704bd1cc942bd74bfdef
fermaciel <104040518+fermaciel@users.noreply.github.com>
2022-04-25 19:46:43 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+console.log("hola");
+
+let precio: number = 450.5;
+let descuento: number = precio * 0.1;
+let precioFinal: number = precio - descuento;
+
+console.log("el precio del producto es de", precio);
+
+console.log("el decuento es de ", descuento);
+
+console.log("el precio final es de", precioFinal);
